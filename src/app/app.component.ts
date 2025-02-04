85f003bfc75d11accffa57ae3c8ac2b6669cd823
cretara <pierdanielecretara@lviiier.com>
2025-02-04 22:39:31 +0100

    Added first components of CRUD application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {EditinsertformComponent} from './features/crud/editinsertform/editinsertform.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EditinsertformComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
